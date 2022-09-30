cceab575f46a6483e557ee0e148875d55d9ab0f8
Alex <belohvostov.belohvostov@gmail.com>
2022-09-30 19:19:10 +0300

    changed BrowserRouter to HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { App } from './App';
-import { theme } from './styles/theme/theme';
-import { store, persistor } from './store';
+import { App } from "./App";
+import { theme } from "./styles/theme/theme";
+import { store, persistor } from "./store";
 
-import { ThemeProvider } from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
+        <HashRouter>
           <CssBaseline />
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </ThemeProvider>
